6af1b6e6d032e04169dba972706fc184a79d67b4
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-06-08 20:45:02 +0530

    Add files via upload

---
+++ Lecture_128 Guess Number Higher or Lower/375. Guess Number Higher or Lower II with recursion.cpp
@@ -0,0 +1,20 @@
+#include <climits>
+
+class Solution {
+    
+    int solve(int start, int end){
+        
+        if(start >= end) return 0;
+        
+        int maxi = INT_MAX;
+        for(int i = start; i <= end; i++){
+            maxi = min(maxi, i + max(solve(start, i-1), solve(i+1, end)));
+        }
+        return maxi;
+    }
+    
+public:
+    int getMoneyAmount(int n) {
+        return solve(1, n);
+    }
+};
